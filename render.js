b69f8f858d8783f8c9032c6dfbaacdf70b7e8eac
Arryan <arryan.zaman@hotmail.com>
2017-11-01 21:43:35 -0400

    added bootstrap

---
+++ render.js
@@ -0,0 +1,6 @@
+let generateBtn = document.getElementById('generateBtnge');
+generateBtn.addEventListener('click', function() {
+	let step1Title = document.createElement("h3");
+	step1Title.innerHTML = "Step 1: Count the frequency of each character";
+	document.getElementById('step-1').append
+});
--- script.js
+++ script.js
@@ -1,61 +1,69 @@
-let input = document.getElementById("input");
+const input = document.getElementById("text-input");
+let keys = {};
+
+let begin = function() {
+    let letters = populateLetters();
+    let tree = populateTree(letters);
+    keys = {};
+    generateKeys(tree, "");
+};
 
 let populateLetters = function() {
-	let letters = [];
-	let str = input.value;
+    let letters = [];
+    let str = input.value;
 
-	for(let i = 0; i < str.length; i++) {
-		let letterIndex = -1;
-		for(let j = 0; j < letters.length; j++) {
-			if(letters[j].letter === str.charAt(i)) {
-				letterIndex = j;
-				break;
-			}
-		}
-		if(letterIndex < 0) {
-			letters.push({'letter':str.charAt(i),'count':1});
-		}
-		else {
-			letters[letterIndex].count++;
-		}
-	}
-	letters.sort(function(a,b) {
-		return b.count - a.count;
-	})
-	return letters;
-}
+    for(let i = 0; i < str.length; i++) {
+        let letterIndex = -1;
+        for(let j = 0; j < letters.length; j++) {
+            if(letters[j].letter === str.charAt(i)) {
+                letterIndex = j;
+                break;
+            }
+        }
+        if(letterIndex < 0) {
+            letters.push({'letter':str.charAt(i),'count':1});
+        }
+        else {
+            letters[letterIndex].count++;
+        }
+    }
+    letters.sort(function(a,b) {
+        return b.count - a.count;
+    })
+    return letters;
+};
 
 let populateTree = function(letters) {
-	while(letters.length > 1) {
-		let right = letters.pop();
-		let left = letters.pop();
-		let newNode = new node(left,right);
-		let newCount = left.count + right.count;
-		letters.push({'letter': newNode,'count': newCount});
-		//swap letters until sorted
-		for(let i = letters.length - 1; i > 0; i--) {
-			if(letters[i].count > letters[i - 1].count) {
-				temp = letters[i];
-				letters[i] = letters[i-1]
-				letters[i-1] = temp;
-			}
-			else break;
-		}
-	}
-	//console.log(letters[0]);
-	return letters[0];
-}
+    while(letters.length > 1) {
+            let right = letters.pop();
+            let left = letters.pop();
+            let newNode = new node(left,right);
+            let newCount = left.count + right.count;
+            letters.push({'letter': newNode,'count': newCount});
+            //swap letters until sorted
+            for(let i = letters.length - 1; i > 0; i--) {
+                if(letters[i].count > letters[i - 1].count) {
+                    temp = letters[i];
+                    letters[i] = letters[i-1]
+                    letters[i-1] = temp;
+                }
+                else break;
+            }
+    }
+    return letters[0];
+};
 
 let generateKeys = function(tree, key) {
-	if(typeof(tree.letter) === "string")
-		console.log(tree.letter + " : " + key);
-	else {
-		generateKeys(tree.letter.left, key + "0");
-		generateKeys(tree.letter.right, key + "1");
-	}
-}
+    if(typeof(tree.letter) === "string") {
+            keys[tree.letter] = key
+    }
+    else {
+            generateKeys(tree.letter.left, key + "0");
+            generateKeys(tree.letter.right, key + "1");
+    }
+};
 
 function node(left,right) {
-	this.left = left;
-	this.right = right;
+    this.left = left;
+    this.right = right;
 }
