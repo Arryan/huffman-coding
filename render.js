01c6fbb11c6b09de66d357867e2d2ab862da3761
Arryan <arryan.zaman@hotmail.com>
2017-11-04 12:48:47 -0400

    broken

--- render.js
+++ render.js
@@ -1,17 +1,19 @@
 let generateBtn = document.getElementById('generate-btn');
 generateBtn.addEventListener('click', function() {
+    counts = [];
+    keys = {};
     let letters = populateLetters();
     let tree = populateTree(letters);
-    keys = {};
     generateKeys(tree, "");
     displayCount();
+    select2();
 });
 
 
 let displayCount = function() {
 	let charsLeft = counts[0].length;
 	let rowCount = Math.ceil(charsLeft / 12);
-
+	document.getElementById('step1-content').innerHTML = "";
 	for(let i = 0; i < rowCount; i++) {
 		let newRow = document.createElement('div');
 		newRow.classList.add('row');
@@ -23,8 +25,20 @@ let displayCount = function() {
 			newCol.classList.add('tree-node');
 			newCol.innerHTML = "<div>" + currentLetter + currentCount + "</div";
 			newRow.appendChild(newCol);
-			document.getElementById("wrapper").appendChild(newRow);
+			//insert after step1-title
+			document.getElementById("step1-content")
+			.appendChild(newRow);
 		}
 		charsLeft -= 12;
 	}
+}
+
+let select2 = function() {
+	let content = document.getElementById("step1-content").cloneNode();
+	content.id = "step2-content";
+	content.innerHTML = document.getElementById("step1-content").innerHTML;
+	let lastRow = content.childNodes[content.childNodes.length - 1];
+	lastRow.childNodes[lastRow.childNodes.length - 1].childNodes[0].style.background = "green";
+	lastRow.childNodes[lastRow.childNodes.length - 2].childNodes[0].style.background = "green";
+	document.getElementById("wrapper").appendChild(content);
 }
